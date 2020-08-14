dd2608776a9e36d947f742f67f0ef881f4e15fcb
Jordan Rocha <70rd23@gmail.com>
2020-08-14 20:17:46 -0300

    Adicionei novo test para a base do projeto

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -6,4 +6,9 @@ describe("Test the root path", () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
   });
+
+  test("Test root body", async () => {
+    const response = await request(app).get("/");
+    expect(response.text).toBe("Hola Mundo");
+  });
 });
